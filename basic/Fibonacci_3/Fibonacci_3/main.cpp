c3a693252a9b15b931dcf4a11824565804f7d94d
sayong.hong <sayong.hong@hanwha.com>
2018-05-12 13:40:36 +0900

    변수명 변경 및 주석 추가

--- basic/Fibonacci_3/Fibonacci_3/main.cpp
+++ basic/Fibonacci_3/Fibonacci_3/main.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-long long fibo_recursion(int _N) {
+long long Fibonacci_Recursion(int _N) {
 	if (_N == 0) return 0;
 	if (_N == 1) return 1;
 
-	return fibo_recursion(_N - 1) + fibo_recursion(_N - 2);
+	return Fibonacci_Recursion(_N - 1) + Fibonacci_Recursion(_N - 2);
 }
 
-long long fibo_dynamic(vector<long long>& buffer, int _N) {
-	if (buffer[_N] != -1) {
-		return buffer[_N];
-	}
+long long Fibonacci_Dynamic(long long buf[], int _N) {
+	// 값을 알고 있으면 바로 그 값 리턴
+	if (buf[_N] != -1) return buf[_N];
 
-	buffer[_N] = fibo_dynamic(buffer, _N - 1) + fibo_dynamic(buffer, _N - 2);
-	return buffer[_N];
+	buf[_N] = Fibonacci_Dynamic(buf, _N - 1) + Fibonacci_Dynamic(buf, _N - 2);
+	
+	return buf[_N];
 }
 
-long long fibo_iteration(vector<long long>& buffer, int _N) {
-	for (int i = 2; i <= _N; i++) {
-		buffer[i] = buffer[i - 1] + buffer[i - 2];
+long long Fibonacci_Loop(long long buf[], int _N) {
+	for (int idx = 2; idx <= _N; idx++) {
+		buf[idx] = buf[idx - 1] + buf[idx - 2];
 	}
-	return buffer[_N];
+	return buf[_N];
 }
 
 int main() {
 	int N;
-	cout << "N: ";
+	cout << "FIBONACCI N: ";
 	cin >> N; cin.get();
 
-	//cout << "Fibonacci Recursion[" << N << "]: " << fibo_recursion(N) << endl;
-
-	vector<long long> buffer(N + 1, -1);
+	long long* buffer = new long long[N + 1];
+	for (int i = 0; i < N + 1; i++) buffer[i] = -1;
 	buffer[0] = 0;
 	buffer[1] = 1;
-	//cout << "Fibonacci Dynamic[" << N << "]: " << fibo_dynamic(buffer, N) << endl;
+	
+	cout << "FIBONACCI Recursion: " << Fibonacci_Recursion(N) << endl;
+	cout << "FIBONACCI Dynamic: " << Fibonacci_Dynamic(buffer, N) << endl;
+	cout << "FIBONACCI Loop: " << Fibonacci_Loop(buffer, N) << endl;
 
-	cout << "Finonacci Iteration[" << N << "]: " << fibo_iteration(buffer, N) << endl;
+	delete buffer;
 
 	getchar();
 	return 0;
