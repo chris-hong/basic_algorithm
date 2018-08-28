6860528aa4b67ebe3299a7d8f4b9b296337bd421
sayong.hong <sayong.hong@hanwha.com>
2018-08-28 09:03:19 +0900

    9663_N-Queen 표준 답안 업로드

--- acmicpc/9663_N-Queen/9663_N-Queen/main.cpp
+++ acmicpc/9663_N-Queen/9663_N-Queen/main.cpp
@@ -1,57 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int SELECT_X[15];
-int SELECT_Y[15];
-int totalCount = 0;		// 중복이 발생하여.. AVL에 적용해서 넣자.
-
-template <typename T>
-struct tNode {
-	T v;
-	struct tNode<T> pLeft;
-	struct tNode<T> pRight;
-};
+bool* SELECT;			// 이미 선택한 INDEX 기록
+bool* CROSS_RIGHT_UP;	// 이미 선택된 右上 대각선 INDEX 기록
+bool* CROSS_RIGHT_DOWN;	// 이미 선택된 右下 대각선 INDEX 기록
+int totalCount = 0;
 
 int ABS(int num) {
 	return num > 0 ? num : -num;
 }
 
-bool IsSameRow(int fromX, int fromY, int toX, int toY) {
-	if (fromX == toX || fromY == toY) return true;
-	else return false;
-}
-
-bool IsCross(int fromX, int fromY, int toX, int toY) {
-	int x = ABS(fromX - toX);
-	int y = ABS(fromY - toY);
-
-	return x == y ? true : false;
-}
-
-bool IsPossible(int x, int y, int depth) {
-	for (int i = 0; i < depth; i++) {
-		if (IsSameRow(SELECT_X[i], SELECT_Y[i], x, y)) return false;
-		if (IsCross(SELECT_X[i], SELECT_Y[i], x, y)) return false;
-	}
-	return true;
-}
- 
-void N_Queen(int depth, int N) {
-	if (N == 1) return;
-
-	if (depth == N) {
+void N_Queen(int depth, int _N) {
+	if (depth == _N) {
 		totalCount += 1;
 		return;
 	}
 
-	for (int x = 0; x < N; x++) {
-		for (int y = 0; y < N; y++) {
-			if (IsPossible(x, y, depth)) {
-				SELECT_X[depth] = x;
-				SELECT_Y[depth] = y;
-				N_Queen(depth + 1, N);
-			}
-		}
+	// 규칙성을 발견해보자
+	// RIGHT_UP : depth + idx 의 값이 모두 같음
+	// RIGHT_DOWN : depth - idx 의 값이 모두 같음 => 음수를 피하기 위하여 (N - 1) - (depth - idx)
+	for (int idx = 0; idx < _N; idx++) {
+		int rightUp = depth + idx;
+		int rightDown = _N - 1 + depth - idx;
+		if (SELECT[idx]) continue;
+		if (CROSS_RIGHT_UP[rightUp]) continue;
+		if (CROSS_RIGHT_DOWN[rightDown]) continue;	
+
+		// 체크하고 DFS
+		SELECT[idx] = true;
+		CROSS_RIGHT_UP[rightUp] = true;
+		CROSS_RIGHT_DOWN[rightDown] = true;
+
+		N_Queen(depth + 1, _N);
+
+		// 언체크 원복(Back-Tracking)
+		SELECT[idx] = false;
+		CROSS_RIGHT_UP[rightUp] = false;
+		CROSS_RIGHT_DOWN[rightDown] = false;
 	}
 }
 
@@ -59,9 +44,21 @@ int main() {
 	int N;
 	cin >> N;
 
+	SELECT = new bool[N];
+	CROSS_RIGHT_UP = new bool[2 * N - 1];
+	CROSS_RIGHT_DOWN = new bool[2 * N - 1];
+
+	fill_n(SELECT, N, false);
+	fill_n(CROSS_RIGHT_UP, 2 * N - 1, false);
+	fill_n(CROSS_RIGHT_DOWN, 2 * N - 1, false);
+
 	N_Queen(0, N);
 
 	cout << totalCount << endl;
+
+	delete SELECT;
+	delete CROSS_RIGHT_UP;
+	delete CROSS_RIGHT_DOWN;
 
 	return 0;
 }
